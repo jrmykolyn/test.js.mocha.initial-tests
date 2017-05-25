b0234459be29e5dfee1cd46eb443d51b72037c62
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-05-25 09:06:23 -0400

    Added 'index.js' file.

---
+++ index.js
@@ -0,0 +1,5 @@
+// --------------------------------------------------
+// INIT
+// --------------------------------------------------
+console.log( 'All tests located in `test/` dir.' );
+console.log( 'Invoke either `mocha` or `npm run test` commands to run tests.' );
